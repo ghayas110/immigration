8eb24c0c7bbf132d151fe2aaedf84dda0675123b
Ghayas Ali <55618250+ghayas110@users.noreply.github.com>
2025-06-02 22:09:53 +0500

    ddd

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-module.exports = {
-  //... other configurations ...
+// Next.js configuration file
+// This file is used to configure various aspects of a Next.js application
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // output: 'export',
   images: {
     domains: ['cdn.sanity.io'],
   },
 }
+ 
+module.exports = nextConfig
